2d3ceaf1c00b8936cdc554b50194b774802a4539
allanbbs <allanbp.br@gmail.com>
2020-05-17 16:55:28 +0100

    ..

--- 3/int1.sql
+++ 3/int1.sql
@@ -1,7 +1,8 @@
 .mode columns
 .headers on
 .nullvalue NULL
-
-SELECT prod_name
-FROM product
-WHERE value>=100;
+#MEDIO/FACIL
+SELECT prod_name,value,description
+FROM product,quantity
+WHERE product.serial_code = quantity.s_code AND quantity.quant>=4
+AND product.cat_id in (SELECT id FROM category WHERE id==4 OR id == 6);
