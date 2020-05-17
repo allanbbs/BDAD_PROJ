5bc2aaf61b413f061fe91d5aa29d812785eaa7ab
allanbbs <allanbp.br@gmail.com>
2020-05-17 20:07:27 +0100

    ..

--- 3/int4.sql
+++ 3/int4.sql
@@ -1,3 +1,8 @@
 .mode columns
 .headers on
 .nullvalue NULL
+
+#MEDIO/FACIL
+SELECT DISTINCT catename,prod_name 
+FROM rating,product,category
+WHERE rating.serial_code == product.serial_code AND rating.rate>7 AND product.cat_id == category.id;
