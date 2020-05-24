8fb07d6a93215ef0ef0aea72cddba0f92506fcd9
carolrosemback <carolinarosemback@gmail.com>
2020-05-24 13:08:03 -0300

    Carol kind of done

--- 3/int10.sql
+++ 3/int10.sql
@@ -4,3 +4,8 @@
 
 
 #DIFICIL
+#Pares de clientes  que compraram o mesmo produto
+
+SELECT c1.name, c1.id, c2.name, c2.id, quantity.s_code
+FROM client AS c1, client AS c2, quantity
+WHERE c1.id != c2.id;
--- 3/int8.sql
+++ 3/int8.sql
@@ -2,9 +2,9 @@
 .headers on
 .nullvalue NULL
 
-#cidades em que foram feitas entregas para empresas
+#cidades em que foram feitas entregas para empresas e seus respectivos clientes 
 
 
-SELECT dname, enterprise
-FROM address, district
-WHERE enterprise == 1 AND address.d_id == district.id
+SELECT DISTINCT dname, enterprise, delivery_destination.client_id
+FROM address, district, orderr, delivery_destination 
+WHERE enterprise == 1 AND address.d_id == district.id AND delivery_destination.address_id == address.id;
