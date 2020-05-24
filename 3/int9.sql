c6f2ba9a5bd4d32b13a654da3e98cb663a19479d
carolrosemback <carolinarosemback@gmail.com>
2020-05-24 18:27:25 -0300

    carol done

--- 3/int9.sql
+++ 3/int9.sql
@@ -2,6 +2,7 @@
 .headers on
 .nullvalue NULL
 
-SELECT  pay_meth.id, order_payment.order_code, order_payment.pay_id 
-FROM  pay_meth, order_payment
-WHERE pay_meth.id == 2
+SELECT  DISTINCT pay_id, orderr.client_id, order_address.address_id, district.dName
+FROM  order_payment, orderr, order_address, delivery_destination, address, district
+WHERE  order_payment.order_code == orderr.order_code AND order_address.order_code == orderr.order_code AND district.id in 
+	(SELECT address.d_id FROM address WHERE delivery_destination.address_id == address.id AND delivery_destination.client_id == orderr.client_id);
